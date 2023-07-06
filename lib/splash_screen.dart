37dbf0d8d426afbd15715d2bf2b8c09319add3b4
Oskaroff <yurakutenko@gmail.com>
2023-07-06 18:17:29 +0300

    first_screen

---
+++ lib/splash_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'main_screen.dart';
+
+class SplashScreenWidget extends StatefulWidget {
+  const SplashScreenWidget({super.key});
+
+  @override
+  State<SplashScreenWidget> createState() => _SplashScreenWidgetState();
+}
+
+class _SplashScreenWidgetState extends State<SplashScreenWidget> {
+  @override
+  initState() {
+    super.initState();
+    _navigateToHome();
+  }
+
+  void _navigateToHome() async {
+    await Future.delayed(const Duration(seconds: 3));
+
+    final navigator = Navigator.of(context);
+    navigator.push(MaterialPageRoute(
+        builder: (BuildContext context) => const MainScreenWidget()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment(0.8, 1),
+              colors: [
+                Color.fromARGB(255, 122, 11, 128),
+                Color(0xff870160),
+                Color(0xffac255e),
+                Color(0xffca485c),
+                Color(0xffe16b5c),
+                Color(0xfff39060),
+                Color(0xffffb56b),
+              ]),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Icon(
+                Icons.add_chart,
+                size: 150,
+                color: Colors.black,
+              ),
+              Text(
+                "Everything in ONE",
+                style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
